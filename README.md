457fb025df4e6a0f93240a3e6c934fb1ce0c32a1
catatsuy <catatsuy@catatsuy.org>
2014-10-09 03:07:13 +0900

    initial

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Acme::2zicon - It's new $module
+
+# SYNOPSIS
+
+    use Acme::2zicon;
+
+# DESCRIPTION
+
+Acme::2zicon is ...
+
+# LICENSE
+
+Copyright (C) catatsuy.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+catatsuy <catatsuy@catatsuy.org>
---
+++ lib/Acme/2zicon.pm
@@ -0,0 +1,39 @@
+package Acme::2zicon;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Acme::2zicon - It's new $module
+
+=head1 SYNOPSIS
+
+    use Acme::2zicon;
+
+=head1 DESCRIPTION
+
+Acme::2zicon is ...
+
+=head1 LICENSE
+
+Copyright (C) catatsuy.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+catatsuy E<lt>catatsuy@catatsuy.orgE<gt>
+
+=cut
+
