a22f83c6ed940b944550964d437934a94fff701c
catatsuy <catatsuy@catatsuy.org>
2014-11-02 22:43:30 +0900

    Checking in changes prior to tagging of version 0.3.1.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 90432c8..4aaf6e4 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,6 +1,9 @@
     Revision history for Perl extension Acme-2zicon
     {{$NEXT}}
    
    +0.3.1 2014-11-02T13:43:25Z
    +    - add Tsurumi Moe's introduction
    +
     0.3 2014-10-31T14:06:17Z
         - add Moe Tsurumi(new member)

--- lib/Acme/2zicon.pm
+++ lib/Acme/2zicon.pm
@@ -6,7 +6,7 @@ use warnings;
 use Carp  qw(croak);
 use DateTime;
 
-our $VERSION = "0.3";
+our $VERSION = "0.3.1";
 
 my @members = qw(
     KinoshitaHiyori
--- lib/Acme/2zicon/Base.pm
+++ lib/Acme/2zicon/Base.pm
@@ -5,7 +5,7 @@ use warnings;
 use DateTime;
 use base qw(Class::Accessor);
 
-our $VERSION = '0.3';
+our $VERSION = '0.3.1';
 
 __PACKAGE__->mk_accessors(qw(
     name_ja
